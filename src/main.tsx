9ea545fc4e3421b4bf52be72c139a29b7daeda6d
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-09-17 14:13:49 +0300

    git init

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/700.css'
-import './index.css'
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
-createRoot(document.getElementById('root')!).render(
+
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
