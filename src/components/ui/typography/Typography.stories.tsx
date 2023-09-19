caa2ddf22bc26c1e98f48a31462af492a5b89882
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-09-20 01:14:55 +0300

    fix displaying text in stories

--- src/components/ui/typography/Typography.stories.tsx
+++ src/components/ui/typography/Typography.stories.tsx
@@ -15,31 +15,59 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const H1: Story = {
-  render: () => <Typography variant={'h1'}>Hello world!</Typography>,
+  render: () => (
+    <Typography color={'dark'} variant={'h1'}>
+      Hello world!
+    </Typography>
+  ),
 }
 
 export const H2: Story = {
-  render: () => <Typography variant={'h2'}>Hello world!</Typography>,
+  render: () => (
+    <Typography color={'dark'} variant={'h2'}>
+      Hello world!
+    </Typography>
+  ),
 }
 
 export const H3: Story = {
-  render: () => <Typography variant={'h3'}>Hello world!</Typography>,
+  render: () => (
+    <Typography color={'dark'} variant={'h3'}>
+      Hello world!
+    </Typography>
+  ),
 }
 
 export const Body1: Story = {
-  render: () => <Typography variant={'body1'}>Hello world!</Typography>,
+  render: () => (
+    <Typography color={'dark'} variant={'body1'}>
+      Hello world!
+    </Typography>
+  ),
 }
 
 export const Body2: Story = {
-  render: () => <Typography variant={'body2'}>Hello world!</Typography>,
+  render: () => (
+    <Typography color={'dark'} variant={'body2'}>
+      Hello world!
+    </Typography>
+  ),
 }
 
 export const Subtitle1: Story = {
-  render: () => <Typography variant={'subtitle1'}>Hello world!</Typography>,
+  render: () => (
+    <Typography color={'dark'} variant={'subtitle1'}>
+      Hello world!
+    </Typography>
+  ),
 }
 
 export const Subtitle2: Story = {
-  render: () => <Typography variant={'subtitle2'}>Hello world!</Typography>,
+  render: () => (
+    <Typography color={'dark'} variant={'subtitle2'}>
+      Hello world!
+    </Typography>
+  ),
 }
 
 export const Link1: Story = {
@@ -59,13 +87,25 @@ export const Link2: Story = {
 }
 
 export const Overline: Story = {
-  render: () => <Typography variant={'overline'}>Hello world!</Typography>,
+  render: () => (
+    <Typography color={'dark'} variant={'overline'}>
+      Hello world!
+    </Typography>
+  ),
 }
 
 export const Large: Story = {
-  render: () => <Typography variant={'large'}>Hello world!</Typography>,
+  render: () => (
+    <Typography color={'dark'} variant={'large'}>
+      Hello world!
+    </Typography>
+  ),
 }
 
 export const Caption: Story = {
-  render: () => <Typography variant={'caption'}>Hello world!</Typography>,
+  render: () => (
+    <Typography color={'dark'} variant={'caption'}>
+      Hello world!
+    </Typography>
+  ),
 }
