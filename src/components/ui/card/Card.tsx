054003f93e9f9ae34c81830a93ff05b0a03d00e4
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-09-21 19:30:01 +0300

    implement card component

--- src/components/ui/card/Card.tsx
+++ src/components/ui/card/Card.tsx
@@ -1,3 +1,21 @@
-export const Card = () => {
-  return <div></div>
+import { ReactNode } from 'react'
+
+import s from './Card.module.scss'
+
+type Props = {
+  children?: ReactNode
+  classNameCard?: string
+  classNameContent?: string
+}
+export const Card = (props: Props) => {
+  const { children, classNameCard, classNameContent } = props
+
+  const finalCardClassName = `${s.card} ${classNameCard ? s[classNameCard] : ''}`
+  const finalContentClassName = `${s.content} ${classNameContent ? s[classNameContent] : ''}`
+
+  return (
+    <div className={finalCardClassName}>
+      <div className={finalContentClassName}>{children}</div>
+    </div>
+  )
 }
