a1d28261ccc15b177cbcff47607b94f4bfe4a8fe
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-09-28 12:07:58 +0300

    implement showing\hiding password in create password card

--- src/components/auth/create-new-password-form/CreateNewPasswordForm.tsx
+++ src/components/auth/create-new-password-form/CreateNewPasswordForm.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react'
+
+import { faEye, faEyeLowVision } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 
@@ -17,6 +21,8 @@ type Props = {
 export const CreateNewPasswordForm = (props: Props) => {
   const { onSubmit } = props
 
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+
   const {
     control,
     handleSubmit,
@@ -30,6 +36,14 @@ export const CreateNewPasswordForm = (props: Props) => {
     onSubmit(data)
   })
 
+  const icon = showPassword ? (
+    <FontAwesomeIcon icon={faEyeLowVision} style={{ color: '#ffffff' }} />
+  ) : (
+    <FontAwesomeIcon icon={faEye} style={{ color: '#ffffff' }} />
+  )
+
+  const inputType = showPassword ? 'text' : 'password'
+
   return (
     <Card classNameCard={s.newPasswordCard}>
       <form className={s.form} onSubmit={onSubmitHandler}>
@@ -37,11 +51,15 @@ export const CreateNewPasswordForm = (props: Props) => {
           Create new password
         </Typography>
         <ControlledInput
+          type={inputType}
           className={s.input}
           control={control}
           name={'password'}
           label={'Password'}
           errorMessage={errors.password?.message}
+          rightSideIcon={icon}
+          setShowPassword={setShowPassword}
+          showPassword={showPassword}
         />
         <Typography className={s.subtitle} variant={'body2'}>
           Create new password and we will send you further instructions to email
