1857ec531330de9d1d2b80ab10ebd1d2ed73168c
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-09-28 11:12:32 +0300

    implement create new password component and its story

---
+++ src/components/auth/create-new-password-form/CreateNewPassword.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { CreateNewPasswordForm } from '@/components/auth/create-new-password-form'
+
+const meta = {
+  title: 'Auth/CreateNewPasswordForm',
+  component: CreateNewPasswordForm,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    onSubmit: {
+      description: 'Send form fields data',
+    },
+  },
+} satisfies Meta<typeof CreateNewPasswordForm>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const CreateNewPassword: Story = {}
---
+++ src/components/auth/create-new-password-form/CreateNewPasswordForm.tsx
@@ -0,0 +1,55 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+import s from './CreateNewPassword.module.scss'
+
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { ControlledInput } from '@/components/ui/controlled/controlledInput'
+import { Typography } from '@/components/ui/typography'
+import { createNewPasswordSchema } from '@/schemes'
+import { CreateNewPasswordFields } from '@/types/common'
+
+type Props = {
+  onSubmit: (values: CreateNewPasswordFields) => void
+}
+
+export const CreateNewPasswordForm = (props: Props) => {
+  const { onSubmit } = props
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateNewPasswordFields>({
+    resolver: zodResolver(createNewPasswordSchema),
+    mode: 'onSubmit',
+  })
+
+  const onSubmitHandler = handleSubmit(data => {
+    onSubmit(data)
+  })
+
+  return (
+    <Card classNameCard={s.newPasswordCard}>
+      <form className={s.form} onSubmit={onSubmitHandler}>
+        <Typography className={s.title} variant={'large'}>
+          Create new password
+        </Typography>
+        <ControlledInput
+          className={s.input}
+          control={control}
+          name={'password'}
+          label={'Password'}
+          errorMessage={errors.password?.message}
+        />
+        <Typography className={s.subtitle} variant={'body2'}>
+          Create new password and we will send you further instructions to email
+        </Typography>
+        <Button variant={'primary'} fullWidth={true}>
+          <Typography variant={'subtitle2'}>Create New Password</Typography>
+        </Button>
+      </form>
+    </Card>
+  )
+}
