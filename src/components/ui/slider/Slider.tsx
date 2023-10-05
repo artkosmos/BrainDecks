e5571e1ad64155a88fbbe073c923b541b47e2e7a
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-10-05 16:00:33 +0300

    implement setting label in slider

--- src/components/ui/slider/Slider.tsx
+++ src/components/ui/slider/Slider.tsx
@@ -5,41 +5,48 @@ import { Typography } from '@/components/ui/typography'
 import { clsx } from 'clsx'
 import s from './Slider.module.scss'
 
-type Props = ComponentPropsWithoutRef<typeof SliderRadix.Root>
+type Props = {
+  label?: string
+} & ComponentPropsWithoutRef<typeof SliderRadix.Root>
 
 export const Slider = forwardRef<ElementRef<typeof SliderRadix.Root>, Props>((props, ref) => {
   const { min = 0, max = 100, value, className, ...rest } = props
 
-  const value1 = value?.length ? value[0] : min
-  const value2 = value?.length ? value[1] : max
+  const startValue = value?.length ? value[0] : min
+  const endValue = value?.length ? value[1] : max
 
   const finalClassName = clsx(s.root, className)
 
   return (
     <div className={s.sliderWrapper}>
-      <div className={s.rectangle}>
-        <Typography variant={'body1'} className={s.textColor}>
-          {value1}
-        </Typography>
-      </div>
-      <SliderRadix.Root
-        ref={ref}
-        max={max}
-        min={min}
-        className={finalClassName}
-        value={value}
-        {...rest}
-      >
-        <SliderRadix.Track className={s.track}>
-          <SliderRadix.Range className={s.range} />
-        </SliderRadix.Track>
-        <SliderRadix.Thumb className={s.thumb} />
-        <SliderRadix.Thumb className={s.thumb} />
-      </SliderRadix.Root>
-      <div className={s.rectangle}>
-        <Typography variant={'body1'} className={s.textColor}>
-          {value2}
-        </Typography>
+      <Typography className={s.label} variant={'body2'}>
+        Number of cards
+      </Typography>
+      <div className={s.slider}>
+        <div className={s.rectangle}>
+          <Typography variant={'body1'} className={s.textColor}>
+            {startValue}
+          </Typography>
+        </div>
+        <SliderRadix.Root
+          ref={ref}
+          max={max}
+          min={min}
+          className={finalClassName}
+          value={value}
+          {...rest}
+        >
+          <SliderRadix.Track className={s.track}>
+            <SliderRadix.Range className={s.range} />
+          </SliderRadix.Track>
+          <SliderRadix.Thumb className={s.thumb} />
+          <SliderRadix.Thumb className={s.thumb} />
+        </SliderRadix.Root>
+        <div className={s.rectangle}>
+          <Typography variant={'body1'} className={s.textColor}>
+            {endValue}
+          </Typography>
+        </div>
       </div>
     </div>
   )
