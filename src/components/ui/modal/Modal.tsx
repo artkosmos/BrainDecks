1f3cfa25750cce42cdbf718986acb572d16c13e7
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-10-05 18:45:35 +0300

    refactor modal component

--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -1,23 +1,25 @@
-import { ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
+import { clsx } from 'clsx'
 import s from './Modal.module.scss'
 
-export type ModalPropsType = {
-  open?: boolean
-  onOpenChange?: (open: boolean) => void
-  children?: ReactNode
-}
+export type ModalProps = ComponentPropsWithoutRef<typeof Dialog.Root> &
+  ComponentPropsWithoutRef<typeof Dialog.Content>
 
-export const Modal = ({ open, onOpenChange, children }: ModalPropsType) => {
+export const Modal = forwardRef<ElementRef<typeof Dialog.Content>, ModalProps>((props, ref) => {
+  const { children, className, title, ...rest } = props
+
+  const contentClassName = clsx(s.content, className)
+
   return (
-    <Dialog.Root open={open} onOpenChange={onOpenChange}>
+    <Dialog.Root {...rest}>
       <Dialog.Portal>
         <Dialog.Overlay className={s.overlay} />
-        <Dialog.Content className={s.content}>
+        <Dialog.Content ref={ref} className={contentClassName} {...rest}>
           {children}
           <Dialog.Close className={s.closeBtn} />
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
   )
-}
+})
