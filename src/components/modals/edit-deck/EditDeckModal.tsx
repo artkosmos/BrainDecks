4507d737e74cc30849d0d173d3f34c2d32264e96
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-10-06 12:58:43 +0300

    implement edit deck modal

---
+++ src/components/modals/edit-deck/EditDeckModal.tsx
@@ -0,0 +1,67 @@
+import { ControlledInput } from '@/components/ui/controlled/controlledInput'
+import { useForm } from 'react-hook-form'
+import { DeckModals, NewDeckNameField } from '@/types/common'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { newDeckNameSchema } from '@/schemes'
+import { ControlledCheckbox } from '@/components/ui/controlled/controlledCheckbox'
+import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
+import { Modal } from '@/components/ui/modal'
+import s from './EditDeckModal.module.scss'
+
+type Props = {
+  open: DeckModals
+  setOpen: (value: DeckModals | null) => void
+  onSubmit: (values: NewDeckNameField) => void
+}
+
+export const EditDeckModal = ({ onSubmit, open, setOpen }: Props) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NewDeckNameField>({
+    resolver: zodResolver(newDeckNameSchema),
+    mode: 'onSubmit',
+  })
+
+  const onSubmitHandler = handleSubmit(data => {
+    onSubmit(data)
+  })
+
+  const cancelModalHandler = () => {
+    setOpen(null)
+  }
+
+  return (
+    <Modal className={s.modal} open={open === DeckModals.UPDATE}>
+      <Typography className={s.title} variant={'h2'}>
+        Edit Deck
+      </Typography>
+      <form onSubmit={onSubmitHandler} className={s.newDeckForm}>
+        <ControlledInput
+          aria-label={'enter new deck name'}
+          className={s.input}
+          control={control}
+          name={'name'}
+          label={'Name Deck'}
+          errorMessage={errors.name?.message}
+        />
+        <ControlledCheckbox
+          className={s.checkbox}
+          label={'Private pack'}
+          control={control}
+          name={'isPrivate'}
+        />
+        <div className={s.buttonArea}>
+          <Button onClick={cancelModalHandler} type={'button'} variant={'secondary'}>
+            <Typography variant={'subtitle2'}>Cancel</Typography>
+          </Button>
+          <Button type={'submit'}>
+            <Typography variant={'subtitle2'}>Save Changes</Typography>
+          </Button>
+        </div>
+      </form>
+    </Modal>
+  )
+}
