5cd6226176607dd90a50810842a8f9f8fb924790
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-10-06 11:51:22 +0300

    implement add new deck modal

---
+++ src/components/modals/add-new-deck/AddNewDeckModal.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { AddNewDeckModal } from '@/components/modals/add-new-deck'
+import { DeckModals } from '@/types/common'
+
+const meta = {
+  title: 'Modals/AddNewDeck',
+  component: AddNewDeckModal,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    onSubmit: {
+      description: 'Send form fields data',
+    },
+  },
+} satisfies Meta<typeof AddNewDeckModal>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const AddNewDeck: Story = {
+  args: {
+    open: DeckModals.CREATE,
+  },
+}
