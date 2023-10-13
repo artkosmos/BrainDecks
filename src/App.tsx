dbf5151b8a3760b9f2e22a187ff9582f4141ea62
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-10-13 12:14:57 +0300

    remove inline styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import { Router } from '@/Router.tsx'
 import { Provider } from 'react-redux'
 import { store } from '@/services/store.ts'
-import { CSSProperties } from 'react'
 import s from './App.module.scss'
 
 export function App() {
-  const tempStyles = {
-    display: 'flex',
-    flexDirection: 'column' as CSSProperties['flexDirection'],
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '50px',
-  }
-
   return (
-    <div style={tempStyles} className={s.appcontainer}>
+    <div className={s.appContainer}>
       <Provider store={store}>
         <Router />
       </Provider>
