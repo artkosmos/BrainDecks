8dfaa701aadd311827d44194224c02f1cff93edb
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-10-27 17:17:02 +0300

    implement skeleton preloader for personal info card

--- src/components/ui/personalInfo/PersonalInformation.tsx
+++ src/components/ui/personalInfo/PersonalInformation.tsx
@@ -14,6 +14,7 @@ import { SignOutIcon } from '@/assets/icons/components/SignOutIcon.tsx'
 import { EditIcon } from '@/assets/icons/components/EditIcon.tsx'
 import { CancelIcon } from '@/assets/icons/components/CancelIcon.tsx'
 import { ControlledFileInput } from '@/components/ui/controlled/controlledFileInput'
+import { PersonalInfoSkeleton } from '@/components/ui/personalInfoSkeleton'
 import s from './PersonalInfo.module.scss'
 
 type Props = {
@@ -23,7 +24,7 @@ type Props = {
   isLoading?: boolean
 }
 
-export const PersonalInformation = ({ onSubmit, userData, logOutFn }: Props) => {
+export const PersonalInformation = ({ onSubmit, userData, logOutFn, isLoading }: Props) => {
   const [editInfo, setEditInfo] = useState<boolean>(false)
   const [editAvatar, setEditAvatar] = useState<boolean>(false)
 
@@ -57,91 +58,95 @@ export const PersonalInformation = ({ onSubmit, userData, logOutFn }: Props) => 
   return (
     <Card className={s.container} aria-label={'profile information'}>
       <Typography variant={'h1'}>Personal Information</Typography>
-      <form onSubmit={onSubmitHandler} className={s.form}>
-        {editAvatar ? (
-          <div className={s.editAvatarWrapper}>
-            <ControlledFileInput
-              className={s.fileInput}
-              control={control}
-              name={'avatar'}
-              id={'avatar'}
-              buttonText={'Choose New Photo'}
-            />
-            <CancelIcon
-              className={`${s.cancelIcon} ${s.avatarCancelIcon}`}
-              onClick={() => cancelHandler('avatar')}
-            />
-          </div>
-        ) : (
-          <div className={s.avatarWrapper}>
-            <Icon className={s.avatar} srcIcon={userData.avatar || userIcon} alt={'avatar'} />
-            <Button
-              variant={'secondary'}
-              className={s.avatarEditButton}
-              onClick={() => setEditAvatar(true)}
-            >
-              <EditIcon width={16} />
-            </Button>
-          </div>
-        )}
+      {!isLoading ? (
+        <form onSubmit={onSubmitHandler} className={s.form}>
+          {editAvatar ? (
+            <div className={s.editAvatarWrapper}>
+              <ControlledFileInput
+                className={s.fileInput}
+                control={control}
+                name={'avatar'}
+                id={'avatar'}
+                buttonText={'Choose New Photo'}
+              />
+              <CancelIcon
+                className={`${s.cancelIcon} ${s.avatarCancelIcon}`}
+                onClick={() => cancelHandler('avatar')}
+              />
+            </div>
+          ) : (
+            <div className={s.avatarWrapper}>
+              <Icon className={s.avatar} srcIcon={userData.avatar || userIcon} alt={'avatar'} />
+              <Button
+                variant={'secondary'}
+                className={s.avatarEditButton}
+                onClick={() => setEditAvatar(true)}
+              >
+                <EditIcon width={16} />
+              </Button>
+            </div>
+          )}
 
-        {editInfo ? (
-          <div className={s.editInfoWrapper}>
-            <div className={s.inputAndCancel}>
+          {editInfo ? (
+            <div className={s.editInfoWrapper}>
+              <div className={s.inputAndCancel}>
+                <ControlledInput
+                  control={control}
+                  name={'name'}
+                  errorMessage={errors.name?.message}
+                  label={'Nick Name'}
+                  autoFocus
+                />
+                <CancelIcon className={s.cancelIcon} onClick={() => cancelHandler('name')} />
+              </div>
               <ControlledInput
                 control={control}
-                name={'name'}
-                errorMessage={errors.name?.message}
-                label={'Nick Name'}
-                autoFocus
+                name={'email'}
+                errorMessage={errors.email?.message}
+                label={'Email'}
               />
-              <CancelIcon className={s.cancelIcon} onClick={() => cancelHandler('name')} />
             </div>
-            <ControlledInput
-              control={control}
-              name={'email'}
-              errorMessage={errors.email?.message}
-              label={'Email'}
-            />
-          </div>
-        ) : (
-          <div className={s.infoWrapper}>
-            <Typography className={s.userName} variant={'h1'}>
-              {userData.name}
-              <EditIcon width={18} className={s.editIcon} onClick={() => setEditInfo(true)} />
-            </Typography>
-            <Typography className={s.email} variant={'body2'}>
-              {userData.email}
-            </Typography>
-          </div>
-        )}
+          ) : (
+            <div className={s.infoWrapper}>
+              <Typography className={s.userName} variant={'h1'}>
+                {userData.name}
+                <EditIcon width={18} className={s.editIcon} onClick={() => setEditInfo(true)} />
+              </Typography>
+              <Typography className={s.email} variant={'body2'}>
+                {userData.email}
+              </Typography>
+            </div>
+          )}
 
-        {(editInfo || editAvatar) && (
-          <Button
-            type={'submit'}
-            className={s.submitButton}
-            aria-label={'save changes'}
-            variant={'primary'}
-            fullWidth={true}
-          >
-            <Typography variant={'subtitle2'}>Save Changes</Typography>
-          </Button>
-        )}
+          {(editInfo || editAvatar) && (
+            <Button
+              type={'submit'}
+              className={s.submitButton}
+              aria-label={'save changes'}
+              variant={'primary'}
+              fullWidth={true}
+            >
+              <Typography variant={'subtitle2'}>Save Changes</Typography>
+            </Button>
+          )}
+        </form>
+      ) : (
+        <PersonalInfoSkeleton />
+      )}
 
-        {!editInfo && !editAvatar && (
-          <Button
-            type={'button'}
-            aria-label={'logout'}
-            className={s.button}
-            variant={'secondary'}
-            fullWidth={true}
-            onClick={() => logOutFn?.()}
-          >
-            <SignOutIcon />
-            <Typography variant={'subtitle2'}>Logout</Typography>
-          </Button>
-        )}
-      </form>
+      {!editInfo && !editAvatar && (
+        <Button
+          type={'button'}
+          aria-label={'logout'}
+          className={s.button}
+          variant={'secondary'}
+          fullWidth={true}
+          onClick={() => logOutFn?.()}
+        >
+          <SignOutIcon />
+          <Typography variant={'subtitle2'}>Logout</Typography>
+        </Button>
+      )}
     </Card>
   )
 }
